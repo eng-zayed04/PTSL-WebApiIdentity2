3ad91f4f35a45187fb4b74d416bed78d24e8464c
TD10 <TD10@DESKTOP-VTV7NAD>
2018-01-07 15:00:04 +0600

    Add project files.

---
+++ WebApiIdentity2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiIdentity2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApiIdentity2/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApiIdentity2.Startup))]
+
+namespace WebApiIdentity2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
